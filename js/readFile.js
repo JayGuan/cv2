16b76677e729a2ad0e071f71de90d26025a8f49f
jayguan <dongchenguan@gmail.com>
2018-07-29 06:16:06 -0700

    added life, travel page, working on outdoor page

---
+++ js/readFile.js
@@ -0,0 +1,18 @@
+readTextFile("file:///Users/jayguan/Desktop/cv2/img/life/fuzhou1.txt");
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
